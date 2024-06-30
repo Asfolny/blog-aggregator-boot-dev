62955915dbf0bb2624db163d53db6efad17fbfad
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-30 16:02:31 +0200

    Add create user both with timestamps and without

---
+++ sql/queries/users.sql
@@ -0,0 +1,9 @@
+-- name: CreateUserFull :one
+INSERT INTO users (id, name, created_at, updated_at)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: CreateUser :one
+INSERT INTO users (id, name)
+VALUES ($1, $2)
+RETURNING *;
